fadb6e115db4c4098b56f6cac0a2d143fff93549
Dak <vectorskeleton@gmail.com>
2022-05-20 03:13:46 -0700

    Pirates

---
+++ Barotrauma/BarotraumaServer/MoreLevelContent/Shared/Store/PirateStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Barotrauma;
+using MoreLevelContent.Shared.Generation;
+
+namespace MoreLevelContent.Shared.Store
+{
+    public class PirateStore : StoreBase<PirateStore>
+    {
+        private List<PirateOutpostDef> pirateOutposts;
+        private List<PirateNPCSetDef> pirateSets;
+
+        public override void Setup()
+        {
+
+        }
+    }
+}
